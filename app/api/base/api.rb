9c6149b35075921e9d40c99036d22262daef6a3c
yuchel-246 <yuya.f1117@icloud.com>
2020-09-01 11:19:24 +0900

    エラーハンドリング・apiテストコード

--- app/api/base/api.rb
+++ app/api/base/api.rb
@@ -1,6 +1,10 @@
 # app/api/base/api.rb
 module Base
   class API < Grape::API
+    
+    route :any, '*path' do
+      error!({ error: 'not_found・URLが不正です(404)' }, 404)
+    end
     mount V1::Root
   end
 end
